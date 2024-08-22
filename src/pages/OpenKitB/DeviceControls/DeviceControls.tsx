f2045ee8123ab50a6009c2cd17ddcb967e62c3f1
hungtran <hungtran01686527618@gmail.com>
2024-08-22 15:11:31 +0700

    Revert "feat: update dashboard"
    This reverts commit 9c7810d68eb7bbadfd46e47c92e636b9d29f8197.

--- src/pages/OpenKitB/DeviceControls/DeviceControls.tsx
+++ src/pages/OpenKitB/DeviceControls/DeviceControls.tsx
@@ -14,77 +14,83 @@ const DeviceControls: React.FC<DeviceControlsProps> = ({
 }) => {
     return (
         <div className={styles.container}>
-            <h1>Device Controls</h1>
+            <h1>Dashboard</h1>
             <div className={styles.dashboard}>
-                <div className={styles.item}>
-                    <div className={styles.insight_item}>
-                        <div className={styles.content}>
-                            <h3>LED</h3>
-                            <div className={styles.ledContent}>
-                                <button id="toggleButton" onClick={toggleLed}>
-                                    {led === 'off' ? 'Bật' : 'Tắt'}
-                                </button>
+                <div className={styles.insights}>
+
+                    <div className={styles.insightItem}>
+                        <div className={styles.insightContent}>
+                            <div className={styles.left}>
+                                <h3>LED</h3>
+                                <div className={styles.ledContent}>
+                                    <button id="toggleButton" onClick={toggleLed}>
+                                        {led === 'off' ? 'Bật' : 'Tắt'}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className={styles.icon}
-                            style={{ color: led === 'off' ? 'green' : 'red' }}
-                        >
-                            <i className="fas fa-lightbulb"></i>
+                            <div
+                                className={styles.icon}
+                                style={{ color: led === 'off' ? 'green' : 'red' }}
+                            >
+                                <i className="fas fa-lightbulb"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.item}>
-                    <div className={styles.insight_item}>
-                        <div className={styles.content}>
-                            <h3>LED</h3>
-                            <div className={styles.ledContent}>
-                                <button id="toggleButton" onClick={toggleLed}>
-                                    {led === 'off' ? 'Bật' : 'Tắt'}
-                                </button>
+
+                    <div className={styles.insightItem}>
+                        <div className={styles.insightContent}>
+                            <div className={styles.left}>
+                                <h3>LED</h3>
+                                <div className={styles.ledContent}>
+                                    <button id="toggleButton" onClick={toggleLed}>
+                                        {led === 'off' ? 'Bật' : 'Tắt'}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className={styles.icon}
-                            style={{ color: led === 'off' ? 'green' : 'red' }}
-                        >
-                            <i className="fas fa-lightbulb"></i>
+                            <div
+                                className={styles.icon}
+                                style={{ color: led === 'off' ? 'green' : 'red' }}
+                            >
+                                <i className="fas fa-lightbulb"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.item}>
-                    <div className={styles.insight_item}>
-                        <div className={styles.content}>
-                            <h3>LED</h3>
-                            <div className={styles.ledContent}>
-                                <button id="toggleButton" onClick={toggleLed}>
-                                    {led === 'off' ? 'Bật' : 'Tắt'}
-                                </button>
+
+                    <div className={styles.insightItem}>
+                        <div className={styles.insightContent}>
+                            <div className={styles.left}>
+                                <h3>LED</h3>
+                                <div className={styles.ledContent}>
+                                    <button id="toggleButton" onClick={toggleLed}>
+                                        {led === 'off' ? 'Bật' : 'Tắt'}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className={styles.icon}
-                            style={{ color: led === 'off' ? 'green' : 'red' }}
-                        >
-                            <i className="fas fa-lightbulb"></i>
+                            <div
+                                className={styles.icon}
+                                style={{ color: led === 'off' ? 'green' : 'red' }}
+                            >
+                                <i className="fas fa-lightbulb"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.item}>
-                    <div className={styles.insight_item}>
-                        <div className={styles.content}>
-                            <h3>LED</h3>
-                            <div className={styles.ledContent}>
-                                <button id="toggleButton" onClick={toggleLed}>
-                                    {led === 'off' ? 'Bật' : 'Tắt'}
-                                </button>
+
+                    <div className={styles.insightItem}>
+                        <div className={styles.insightContent}>
+                            <div className={styles.left}>
+                                <h3>LED</h3>
+                                <div className={styles.ledContent}>
+                                    <button id="toggleButton" onClick={toggleLed}>
+                                        {led === 'off' ? 'Bật' : 'Tắt'}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className={styles.icon}
-                            style={{ color: led === 'off' ? 'green' : 'red' }}
-                        >
-                            <i className="fas fa-lightbulb"></i>
+                            <div
+                                className={styles.icon}
+                                style={{ color: led === 'off' ? 'green' : 'red' }}
+                            >
+                                <i className="fas fa-lightbulb"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
