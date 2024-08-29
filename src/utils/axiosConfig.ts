a305e1cdf9ee9ca359f7cacfb85c86b52af18731
dinhhuyhoang25032002 <hoangdinh2503@gmail.com>
2024-08-29 10:06:49 +0700

    openlab-fe-1

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,15 +1,18 @@
 import axios from 'axios';
-
-
 const axiosPublic = axios.create({
-    baseURL:'https://openlab.com.vn',
-    withCredentials:true
+    baseURL: 'https://openlab.com.vn',
+    withCredentials: true
 })
-
 
 export const axiosPrivate = axios.create({
-    baseURL:'',
-    withCredentials:true
+    baseURL: '',
+    withCredentials: true,
+    headers: { "Content-Type": "application/json" },
 })
+
+axiosPublic.interceptors.response.use(
+    (response) => response.data,
+    (error) => { return Promise.reject(error) }
+)
 
 export default axiosPublic;
