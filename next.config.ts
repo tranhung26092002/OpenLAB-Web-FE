a52fc610dff95d8064d557569aa3e1e869214354
Tran Van Hung <hungtran01686527618@gmail.com>
2025-01-26 01:00:43 +0700

    feat: fix config webpack

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,21 @@
-import type { NextConfig } from "next";
+const path = require('path');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
-
       },
     ],
+  },
+  webpack: (config: { resolve: { alias: any; }; }) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '~': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
